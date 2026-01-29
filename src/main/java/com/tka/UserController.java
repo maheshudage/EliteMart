c004e401cf1c65d797985c5d9fd7e65974941234
maheshudage <udagemahesh89@gmail.com>
2026-01-29 13:48:51 +0530

    Update UserController.java

--- src/main/java/com/tka/UserController.java
+++ src/main/java/com/tka/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -188,4 +189,5 @@ public class UserController {
 	}
 
 }
+
 
